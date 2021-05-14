1eae0e0064fa1619a2592265b47dccdd02e59a1f
matben001 <44042030+matben001@users.noreply.github.com>
2021-05-13 20:34:59 -0400

    Update getService.py
    fix for python 3

--- getService.py
+++ getService.py
@@ -2,7 +2,7 @@ import sys
 from bluepy.btle import UUID, Peripheral
 
 if len(sys.argv) != 2:
-  print "Fatal, must pass device address:", sys.argv[0], "<device address="">"
+  print("Fatal, must pass device address:", sys.argv[0], "<device address="">")
   quit()
 
 p = Peripheral(sys.argv[1],"random")
@@ -11,4 +11,4 @@ services=p.getServices()
 
 #displays all services
 for service in services:
-   print service
+   print(service)
